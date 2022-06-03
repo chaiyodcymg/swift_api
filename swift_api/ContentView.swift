da68f880875bd29472c36e05fdd487b271d5dd63
Chaiyod Malingam <chaiyod.cymg@gmail.com>
2022-06-04 01:45:18 +0700

    Initial Commit

---
+++ swift_api/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift_api
+//
+//  Created by Chaiyod Mali-ngam on 4/6/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift_api/swift_apiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_apiApp.swift
+//  swift_api
+//
+//  Created by Chaiyod Mali-ngam on 4/6/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct swift_apiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
